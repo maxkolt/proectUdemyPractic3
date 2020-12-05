0c252e9e171027efcaca710f8a3642c5d3c739b1
maxkolt <12345kolt@gmail.com>
2020-12-05 17:05:54 +0300

    работа с табами завершена

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,10 @@
 import "./slider";
+import modals from "./modules/modals";
+import tabs from "./modules/tabs";
 
-console.log(1);
+
+window.addEventListener('DOMContentLoaded', () => {
+    modals();
+    tabs('.glazing_slider','.glazing_block', '.glazing_content', 'active')
+    tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', '.after_click')
+});
