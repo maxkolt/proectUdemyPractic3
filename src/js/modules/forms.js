f972cd60f7a6e74932389cc3542513ff888cae62
maxkolt <12345kolt@gmail.com>
2020-12-05 17:25:32 +0300

    работа с формами вода

---
+++ src/js/modules/forms.js
@@ -0,0 +1,23 @@
+import checkNumInputs from './checkNumInputs';
+
+const forms = (state) => {
+    const form = document.querySelectorAll('form'),
+        inputs = document.querySelectorAll('input');
+
+    checkNumInputs('input[name="user_phone"]');
+
+    const message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Скоро мы с вами свяжемся',
+        failure: 'Что-то пошло не так...'
+    };
+
+    const postData = async (url, data) => {
+        document.querySelector('.status').textContent = message.loading;
+        let res = await fetch(url, {
+            method: "POST",
+            body: data
+        });
+
+        return await res.text();
+    };
