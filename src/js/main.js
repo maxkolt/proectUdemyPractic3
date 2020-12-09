ecd5681c991a85e8c74f6c1198afb2e451fe8956
maxkolt <12345kolt@gmail.com>
2020-12-09 15:38:48 +0300

    Работа с формой калькулятором

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,9 @@ import forms from "./modules/forms";
 
 window.addEventListener('DOMContentLoaded', () => {
     "use strict";
+
+    let modalState = {}
+
     modals();
     tabs('.glazing_slider','.glazing_block', '.glazing_content', 'active')
     tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', '.after_click')
---
+++ src/js/modules/changeModalState.js
@@ -0,0 +1,10 @@
+const changeModalState = (state) => {
+    const windowForm = document.querySelectorAll('.balcon_icons_img'),
+        windowWidth = document.querySelectorAll('#width'),
+        windowHeight = document.querySelectorAll('#height'),
+        windowType = document.querySelectorAll('#view_type'),
+        windowProfile = document.querySelectorAll('.checkbox');
+
+};
+
+export default changeModalState;
